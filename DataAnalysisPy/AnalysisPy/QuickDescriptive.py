f6876a987e940f3e8837840a86e86a371133819e
Thomas <t.pernetcoudrier@gmail.com>
2019-07-22 12:49:04 +0200

    Update QuickDescriptive.py

--- DataAnalysisPy/AnalysisPy/QuickDescriptive.py
+++ DataAnalysisPy/AnalysisPy/QuickDescriptive.py
@@ -379,28 +379,28 @@ def grid_search(df):
 	"""
 
 	return qgrid.show_grid(df,
-				grid_options = {
-	# SlickGrid options
-	'fullWidthRows': True,
-	'syncColumnCellResize': True,
-	'forceFitColumns': False,
-	'defaultColumnWidth': 150,
-	'rowHeight': 28,
-	'enableColumnReorder': True,
-	'enableTextSelectionOnCells': True,
-	'editable': False,
-	'autoEdit': False,
-	'explicitInitialization': True,
+				grid_options={
+                        # SlickGrid options
+                        'fullWidthRows': False,
+                        'syncColumnCellResize': False,
+                        'forceFitColumns': False,
+                        'defaultColumnWidth': 150,
+                        'rowHeight': 28,
+                        'enableColumnReorder': False,
+                        'enableTextSelectionOnCells': True,
+                        'editable': True,
+                        'autoEdit': False,
+                        'explicitInitialization': True,
 
-	# Qgrid options
-	'maxVisibleRows': 10,
-	'minVisibleRows': 8,
-	'sortable': True,
-	'filterable': True,
-	'highlightSelectedCell': True,
-	'highlightSelectedRow': True
-}
-			   )
+                        # Qgrid options
+                        'maxVisibleRows': 10,
+                        'minVisibleRows': 8,
+                        'sortable': True,
+                        'filterable': True,
+                        'highlightSelectedCell': False,
+                        'highlightSelectedRow': True
+                    }
+                    )
 
 
 #############################################################################
@@ -4326,11 +4326,11 @@ dic_scatterg2 = False, dic_cat = False):
 	with wid_fe:
 		display(select_fe_eventHandler(df = dataframe))
 
-	tab = widgets.Tab([wid_quick, wid_gridshow,  wid_ts, wid_cont, wid_high,
+	tab = widgets.Tab([wid_gridshow, wid_quick,  wid_ts, wid_cont, wid_high,
 	 wid_highLow, wid_scatter, wid_scatter1, wid_scatter2, wid_cat, wid_fe])
 
-	tab.set_title(0, 'Quick description')
-	tab.set_title(1, 'Filter dataset')
+	tab.set_title(0, 'Filter dataset')
+	tab.set_title(1, 'Quick description')
 	tab.set_title(2, 'Time Serie plots')
 	tab.set_title(3, 'Low dimensional group')
 	tab.set_title(4, 'High dimensional group')
